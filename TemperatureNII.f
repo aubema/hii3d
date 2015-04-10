8d6715d02bc7bcce54e1ae60cb250071b3dd8362
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2015-04-09 23:57:57 -0400

    mise a jour

--- TemperatureNII.f
+++ TemperatureNII.f
@@ -1,11 +1,11 @@
-       subroutine temperatureNII(NIIra,Ne,aptmp)
-       real temp,NIIra,NIIrca,Ne, aptmp
+       subroutine temperatureNII(NIIra,Nel,aptmp)
+       real temp,NIIra,NIIrca,Nel, aptmp
        integer i
        temp=6000.
 c       print*,NIIra
        do i=1,200
        temp=temp+100.
-       NIIrca=6.91*exp(2.5e4/temp)/(1.+2.5e-3*(Ne/temp**0.5))
+       NIIrca=6.91*exp(2.5e4/temp)/(1.+2.5e-3*(Nel/temp**0.5))
 c       print*,temp, NIIra
           if (NIIrca.le.NIIra) then 
           aptmp=temp
