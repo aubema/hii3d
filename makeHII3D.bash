e34b0d0f8654f966d06817d544ed3cd6e1541871
aubema <aubema@localhost>
2016-05-18 13:59:26 -0400

    compilation

--- makeHII3D.bash
+++ makeHII3D.bash
@@ -1,3 +1,5 @@
+rm -fr bin
+mkdir bin
 gfortran -mcmodel=medium -Wall -fcheck=all -g -fbacktrace -ffpe-trap=zero,overflow,underflow prog-hii3d-v2.f prog-CleanSIINII.f prog-extrant2d.f  prog-interSII.f prog-temperatureNII.f prog-dblshell.f prog-en-sigma.f prog-squaredata.f prog-moysigma.f prog-gaussienne.f prog-writeIFrIT.f -o bin/prog-hii3d
 gfortran prog-simul-ratio.f prog-extrant2d.f -o bin/prog-simul-ratio
 gfortran prog-rms.f prog-intrants2d.f -o bin/prog-rms
